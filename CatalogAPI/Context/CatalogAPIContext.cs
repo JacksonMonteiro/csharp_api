331a2232b6808b4ed605c5fcfa86b5214a62cdaa
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-08-25 17:02:00 -0300

    Db Context class created

---
+++ CatalogAPI/Context/CatalogAPIContext.cs
@@ -0,0 +1,11 @@
+﻿using CatalogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogAPI.Context {
+    public class CatalogAPIContext : DbContext {
+        public CatalogAPIContext(DbContextOptions<CatalogAPIContext> options) : base(options) {}
+
+        public DbSet<Category>? categories { get; set; }
+        public DbSet<Product>? products { get; set; }
+    }
+}
