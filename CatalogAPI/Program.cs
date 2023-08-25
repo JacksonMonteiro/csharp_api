6b72d75ba8089f01814a0de26af3ee8aa73ab79f
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-08-25 17:07:34 -0300

    Registering dbcontext in program

--- CatalogAPI/Program.cs
+++ CatalogAPI/Program.cs
@@ -1,15 +1,19 @@
+using CatalogAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Registering Database
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CatalogAPIContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure HTTP
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
